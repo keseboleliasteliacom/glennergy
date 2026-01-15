14f05336ec514c666ea3026880be94d39ab2b774
kesebol.elias@telia.com <kesebol.elias@telia.com>
2026-01-15 14:32:50 +0100

    Added code for fetcher from old project

---
+++ Libs/Fetcher.h
@@ -0,0 +1,17 @@
+#ifndef FETCHER_H
+#define FETCHER_H
+
+#include <stddef.h>
+
+typedef struct{
+    char *data;
+    size_t size;
+}CurlResponse;
+
+int Curl_Initialize(CurlResponse *_Response);
+
+CurlResponse* Curl_HTTPGet(CurlResponse *_Response, char* url);
+
+void Curl_Dispose(CurlResponse* _Response);
+
+#endif // CURL_H
