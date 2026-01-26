0b45d44348103fe74c9208eb033f50521819c823
Axel <axel_lindstrom_98@hotmail.com>
2026-01-26 11:06:40 +0100

    Fetcher now uses curl, can get data from any api URL

--- Libs/Fetcher.c
+++ Libs/Fetcher.c
@@ -3,17 +3,19 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Förr hade vi att Init skapar en malloc CurlResponse och returnerar pekaren.
+// Kan inte detta ligga på stacken istället?
+// Nu har vi att Init tar en pekare till CurlResponse och initierar den.
+// Det betyder att anroparen måste skapa CurlResponse på stacke eller heapen själv, sedan kalla denna för att initiera.
 int Curl_Initialize(CurlResponse *_Response)
 {
-    CurlResponse *response = (CurlResponse *)malloc(sizeof(CurlResponse));
-
-    if (response == NULL)
+    if (!_Response)
     {
         return -1;
     }
 
-    response->data = NULL;
-    response->size = 0;
+    _Response->data = NULL;
+    _Response->size = 0;
 
     return 0;
 }
@@ -39,30 +41,35 @@ size_t Curl_WriteCallback(void *contents, size_t size, size_t nmemb, void *userp
 
 int Curl_HTTPGet(CurlResponse *_Response, char *url)
 {
-    CURL *curl;
-    CURLcode res;
-
-    curl = curl_easy_init();
+    CURL *curl = curl_easy_init();
+    
     if (!curl)
     {
         free(_Response->data);
         return -1;
     }
-
+    
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Curl_WriteCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)_Response);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, _Response);
+    //curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)_Response);
 
-    res = curl_easy_perform(curl);
+    CURLcode res = curl_easy_perform(curl);
     if (res != CURLE_OK)
     {
         fprintf(stderr, "Curl_HTTPGet: curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        free(_Response->data);
-        _Response->data = NULL;
-        free(_Response);
-        _Response = NULL;
         curl_easy_cleanup(curl);
         return -2;
+    }
+
+    long http_code = 0;
+    // Hämta HTTP-responskoden
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    if (http_code != 200)
+    {
+        fprintf(stderr, "Curl_HTTPGet: HTTP request failed with code %ld\n", http_code);
+        curl_easy_cleanup(curl);
+        return -3;
     }
 
     curl_easy_cleanup(curl);
@@ -71,12 +78,13 @@ int Curl_HTTPGet(CurlResponse *_Response, char *url)
 
 void Curl_Dispose(CurlResponse *_Response)
 {
-    if (_Response == NULL)
+    if (!_Response)
         return;
 
     if (_Response->data != NULL)
         free(_Response->data);
 
-    free(_Response);
-    _Response = NULL;
+    //free(_Response->data);
+    _Response->data = NULL;
+    _Response->size = 0;
 }
