13fe167dc584133b704d8f406d2802aefe2785e6
Axel <axel_lindstrom_98@hotmail.com>
2026-01-26 11:09:35 +0100

    Spotpris struct

--- Libs/API/Spotpris.h
+++ Libs/API/Spotpris.h
@@ -1,17 +1,23 @@
 #ifndef SPOTPRIS_H
 #define SPOTPRIS_H
 
+// TIme eller inte?
 
-typedef struct{
+typedef struct {
+    char time_start[32];
+    char time_end[32];
+    double sek_per_kwh;
+    double eur_per_kwh;
+    double exchange_rate;
+} SpotPriceEntry;
 
-}Time;
 
 typedef struct
 {
-    char* area;
-    float price_sek;
-    Time* time;
-} Spotpris;
+    char* area[4]; // Prisklasser/områden: "SE1", "SE2", "SE3", "SE4"
+    size_t count; // Kommer troligtvis vara 96 kvartar
+    SpotPriceEntry *entries;
+} DagligSpotpris;
 
 
 #endif
