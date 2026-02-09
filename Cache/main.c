919ba574325b79292394235784132ba90a5b3cee
Axel <axel_lindstrom_98@hotmail.com>
2026-02-09 10:22:38 +0100

    Cleaned up

--- Cache/main.c
+++ Cache/main.c
@@ -16,26 +16,38 @@ int main()
 {
     InputCache inputCache;
     bool WorkDone = false;
+    
+    //mkfifo(FIFO_ALGORITHM_WRITE, 0666);
+    
+    AllaSpotpriser spotpris_test;
 
-    mkfifo(FIFO_ALGORITHM_WRITE, 0666);
-    /*
-        int meteo_fd_read = open(FIFO_METEO_READ, O_RDONLY);
 
-        if (meteo_fd_read < 0)
-        {
-            printf("Failed to open file: %s\n", FIFO_METEO_READ);
-            return -1;
-        }
-    */
+    // --- METEO ---
+    
 
+
+    // --- SPOTPRIS ---
     int spotpris_fd_read = open(FIFO_SPOTPRIS_READ, O_RDONLY);
-
+    
     if (spotpris_fd_read < 0)
     {
         printf("Failed to open file: %s\n", FIFO_SPOTPRIS_READ);
         return -2;
     }
+    
+    ssize_t bytesRead = Pipes_ReadBinary(spotpris_fd_read, &spotpris_test, sizeof(AllaSpotpriser));
+ 
+    if(bytesRead > 0)
+    {
+        printf("Got new data %zd\n", bytesRead);
+    }
+ 
+ 
+    AllaSpotpriser_Print(&spotpris_test);
+ 
+    InputCache_SaveSpotpris(&spotpris_test);
 
+    // --- ALGORITM --- 
    /* int algorithm_fd_write = open(FIFO_ALGORITHM_WRITE, O_WRONLY);
 
     if (algorithm_fd_write < 0)
@@ -44,53 +56,7 @@ int main()
         return -3;
     }*/
 
-    AllaSpotpriser spotpris_test;
-
-
-    ssize_t bytesRead = Pipes_ReadBinary(spotpris_fd_read, &spotpris_test, sizeof(AllaSpotpriser));
-
-    if(bytesRead > 0)
-    {
-        printf("Got new data %zd\n", bytesRead);
-    }
-
-
-    AllaSpotpriser_Print(&spotpris_test);
-
-    InputCache_SaveSpotpris(&spotpris_test);
-    /*
-    for (int i = 0; i < 4; i++)
-    {
-        AllaSpotpriser_Print(&spotpris_test);
-    }
-    */
-
-    //ssize_t bytesWritten = Pipes_WriteBinary(algorithm_fd_write, &spotpris_test, sizeof(DagligSpotpris));
-        
-
-    /* Disabled while testing READ functionality
-    // Mocka ta emot meteo
-    MeteoData meteo;
-    meteo.temp = 5.5;
-    inputCache.meteoData = meteo;
-
-    // Mocka en tom spotpris data
-    DagligSpotpris spotpris;
-    inputCache.spotprisData = spotpris;
-
-    // Läsa in user data/fastighetsdata från json.fil
-    // Skip for now
-    */
-
-    // Write to named pipe
-    // Disabled while testing READ functionality
-
-    //close(algorithm_fd_write);
-
-    // close(meteo_fd_read);
     close(spotpris_fd_read);
-
-    // För att testa detta, köra "cat /tmp/fifo_algoritm_write" som en ny process
 
     return 0;
 }
