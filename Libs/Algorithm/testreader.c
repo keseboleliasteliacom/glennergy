5dc3024a24776303351df26284bffc9695e186b9
kesebol.elias@telia.com <kesebol.elias@telia.com>
2026-03-01 15:04:12 +0100

    Matching spotpris to meteo data

--- Libs/Algorithm/testreader.c
+++ Libs/Algorithm/testreader.c
@@ -17,12 +17,6 @@
 
 int test_reader()
 {
-    char *time_stamps[] = {
-        "%s",
-        "%s",
-        "%s",
-        "%s",
-        "2024-06-01T01:00:00Z"};
     InputCache_t *cache = malloc(sizeof(InputCache_t));
     if (!cache)
     {
@@ -55,22 +49,6 @@ int test_reader()
         }
         LOG_INFO("Received from cache Meteo: %zu HomeSystem: %zu price areas: %zu", cache->meteo_count, cache->home_count, sizeof(cache->spotpris.count) / sizeof(cache->spotpris.count[0]));
 
-        char time_buffer[32];
-        snprintf(time_buffer, sizeof(time_buffer), cache->meteo[0].sample[0].time_start);
-        char *hour = time_buffer + 11;   // Move pointer to the hour part
-        hour[2] = '\0';                  // Null-terminate after the hour
-        char *minute = time_buffer + 14; // Move pointer to the minute part
-        minute[2] = '\0';                // Null-terminate after the minute
-
-        for (size_t i = 0; i < cache->meteo_count; i++)
-        {
-            for (size_t j = 0; j < 2 && j < KVARTAR_TOTALT; j++)
-            {
-                printf("time: %s, temp: %.2f\n",
-                       cache->meteo[i].sample[j].time_start,
-                       cache->meteo[i].sample[j].temp);
-            }
-        }
 
         const char *area_names[AREA_COUNT] = {"SE1", "SE2", "SE3", "SE4"};
 
@@ -88,16 +66,9 @@ int test_reader()
 
             for (size_t entry = 0; entry < show_count; entry++)
             {
-                char time_buffer_spot[32];
-                snprintf(time_buffer_spot, sizeof(time_buffer_spot), cache->spotpris.data[area_idx][entry].time_start);
-                char *hour_spot = time_buffer_spot + 11;   // Move pointer to the hour part
-                hour_spot[2] = '\0';                       // Null-terminate after the hour
-                char *minute_spot = time_buffer_spot + 14; // Move pointer to the minute part
-                minute_spot[2] = '\0';                     // Null-terminate after the minute
-
-                if (strncmp(hour, hour_spot, 2) == 0 && strncmp(minute, minute_spot, 2) == 0)
+                if (strncmp(cache->meteo[0].sample[0].time_start, cache->spotpris.data[area_idx][entry].time_start, 16) == 0)
                 {
-                    spot_index = entry; // Store the index of the matching spotpris entry
+                    spot_index = entry;
                 }
             }
 
@@ -109,29 +80,13 @@ int test_reader()
                 {
                     for (size_t entry = spot_index; entry < show_count; entry++)
                     {
-                        char time_buffer_spot[32];
-                        snprintf(time_buffer_spot, sizeof(time_buffer_spot), cache->spotpris.data[area_idx][entry].time_start);
-                        char *hour_spot = (char *)time_buffer_spot + 11;   // Move pointer to the hour part
-                        hour_spot[2] = '\0';                               // Null-terminate after the hour
-                        char *minute_spot = (char *)time_buffer_spot + 14; // Move pointer to the minute part
-                        minute_spot[2] = '\0';
-
-
                         printf("Area: %s, time: %s, price: %.2f SEK/kWh,\n",
                                area_names[area_idx],
                                cache->spotpris.data[area_idx][entry].time_start,
                                cache->spotpris.data[area_idx][entry].sek_per_kwh);
                         for (size_t j = 0; j < 96; j++)
                         {
-
-                            char time_buffer[32];
-                            snprintf(time_buffer, sizeof(time_buffer), cache->meteo[i].sample[j].time_start);
-                            char *hour = time_buffer + 11;   // Move pointer to the hour part
-                            hour[2] = '\0';                  // Null-terminate after the hour
-                            char *minute = time_buffer + 14; // Move pointer to the minute part
-                            minute[2] = '\0';                // Null-terminate after the minute
-
-                            if (strncmp(hour, hour_spot, 2) == 0 && strncmp(minute, minute_spot, 2) == 0)
+                            if (strncmp(cache->meteo[i].sample[j].time_start, cache->spotpris.data[area_idx][entry].time_start, 16) == 0)
                             {
                                 average_WindowLow_test(&cache->spotpris.data[area_idx][entry], stats.area[area_idx].q25, stats.area[area_idx].q75);
                                 printf("  Matched time: %s, temp: %.2f °C, GHI: %.2f W/m², City: %s\n",
